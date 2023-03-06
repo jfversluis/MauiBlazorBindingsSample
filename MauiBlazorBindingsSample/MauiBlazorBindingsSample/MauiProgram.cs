c916d43168fb7184f2314070aee04909e8a62f87
Gerald Versluis <gerald.versluis@microsoft.com>
2023-03-06 11:25:51 +0100

    Initial commit

---
+++ MauiBlazorBindingsSample/MauiBlazorBindingsSample/MauiProgram.cs
@@ -0,0 +1,17 @@
+﻿using BlazorBindings.Maui;
+
+namespace MauiBlazorBindingsSample {
+    public static class MauiProgram {
+        public static MauiApp CreateMauiApp() {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<BlazorBindingsApplication<AppShell>>()
+                .UseMauiBlazorBindings()
+                .ConfigureFonts(fonts => {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            return builder.Build();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauiBlazorBindingsSample
+ Sample code to demonstrate how to get started with BlazorBindings.Maui in .NET MAUI
